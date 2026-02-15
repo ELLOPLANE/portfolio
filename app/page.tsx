4948fd3965d13dd730539d109f71a4e4fe7598a4
ELLOPLANE <kohrenes@gmail.com>
2026-02-15 23:12:46 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import Hero from './components/Hero';
+
 import About from './components/About';
 import Experiences from './components/Experiences';
 import Contact from './components/Contact';
 
 export default function Home() {
   return (
-    <>
-      <Hero />
+  
       <About />
       <Experiences />
       <Contact />
